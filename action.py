1c5066c5277cd72990dacf2f7fb585fe4f61f903
Minoplhy <zebr@m21.kylz.nl>
2021-08-02 18:57:36 +0700

    Create action.py

---
+++ action.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append('/filters-maker/')
+
+import crawler
+import maker_rpz
+import maker_domains
+
+input = '/repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION/domains.txt'
+excluded = '/repros/Resources/excluded.txt'
+rpz_locat = '/repros/$INPUT_DESTINATION_FOLDER/$INPUT_DESTINATION_VERSION/rpz.txt'
+
+crawler.download_filters("https://dbl.oisd.nl/" ,input)
+crawler.download_filters("https://hosts.netlify.app/Pro/rpz.txt" ,input)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/dns.txt" ,input)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-tracker.txt" ,input)
+crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-original.txt" ,input)
+crawler.download_filters("https://filters.kylz.nl/RPZ/stevenblack/f-s.txt" ,input)
+crawler.download_filters("https://filters.kylz.nl/RPZ/someonewhocares/rpz.txt" ,input)
+crawler.download_filters("https://urlhaus.abuse.ch/downloads/rpz/" ,input)
+crawler.download_filters("https://github.com/easylist/easylist/raw/master/easylist/easylist_adservers.txt" ,input)
+crawler.filtering(input)
+crawler.filteringcon(input)
+crawler.killingdup(input)
+maker_rpz.RPZbuilding(excluded, input, rpz_locat)
+maker_domains.domainsbuilding(excluded, input)

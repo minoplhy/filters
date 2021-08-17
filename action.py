79fbd31edbab0830a1ecbb5af83a3d08a829284e
Minoplhy <zebr@m21.kylz.nl>
2021-08-17 11:46:16 +0700

    Update action.py

--- action.py
+++ action.py
@@ -20,6 +20,24 @@ crawler.download_filters("https://filters.kylz.nl/RPZ/adguard/cname-original.txt
 crawler.download_filters("https://filters.kylz.nl/RPZ/stevenblack/f-s.txt" ,incoming)
 crawler.download_filters("https://filters.kylz.nl/RPZ/someonewhocares/rpz.txt" ,incoming)
 crawler.download_filters("https://urlhaus.abuse.ch/downloads/rpz/" ,incoming)
+crawler.filtering(incoming)
+crawler.filteringcon(incoming)
+crawler.killingdup(incoming)
+crawler.excluded(excluded, incoming)
+crawler.blankremover(incoming)
+crawler.sort(incoming)
+maker_rpz.RPZbuilding(excluded, incoming, rpz_locat)
+maker_hosts.hostsbuilding(excluded, incoming, hosts_locat)
+maker_abp.ABPbuilding(excluded, incoming, abp_locat)
+maker_domains.domainsbuilding(excluded, incoming)
+
+incoming = "/reprwiki/Private-build/veneto/domains.txt"
+excluded = "/repros/Resources/excluded.txt"
+rpz_locat = "/reprwiki/Private-build/veneto/rpz.txt"
+hosts_locat = "/reprwiki/Private-build/veneto/hosts.txt"
+abp_locat = "/reprwiki/Private-build/veneto/adblock.txt"
+
+crawler.clear_old_files(incoming)
 crawler.download_filters("https://block.energized.pro/ultimate/formats/rpz.txt" ,incoming)
 crawler.download_filters("https://blokada.org/mirror/v5/exodusprivacy/standard/hosts.txt" ,incoming)
 crawler.download_filters("https://github.com/minoplhy/filters/raw/main/Resources/blocked.txt" ,incoming)

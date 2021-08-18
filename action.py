aea9e31fcabdb4c030021ba0401b054c395fb8f3
Minoplhy <zebr@m21.kylz.nl>
2021-08-18 12:57:00 +0700

    Update action.py

--- action.py
+++ action.py
@@ -46,6 +46,7 @@ crawler.download_filters("https://blokada.org/mirror/v5/exodusprivacy/standard/h
 crawler.download_filters("https://github.com/minoplhy/filters/raw/main/Resources/blocked.txt" ,incoming)
 crawler.download_filters("https://github.com/crazy-max/WindowsSpyBlocker/raw/master/data/hosts/spy.txt" ,incoming)
 crawler.download_filters("https://energized.pro/extensions/xtreme/formats/rpz.txt" ,incoming)
+crawler.download_filters("https://rpz.oisd.nl" ,incoming)
 crawler.filtering(incoming)
 crawler.filteringcon(incoming)
 crawler.killingdup(incoming)

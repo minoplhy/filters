bea498d69138c3b683baa63af4fd5e01a29d309c
Minoplhy <zebr@m21.kylz.nl>
2021-08-20 15:05:33 +0700

    Update action.py

--- action.py
+++ action.py
@@ -14,6 +14,7 @@ excluded = "/repros/Resources/excluded.txt"
 rpz_locat = "/reprwiki/Private-build/ucate/rpz.txt"
 hosts_locat = "/reprwiki/Private-build/ucate/hosts.txt"
 abp_locat = "/reprwiki/Private-build/ucate/adblock.txt"
+Version = "UCATE"
 os.makedirs('/reprwiki/Private-build/ucate',exist_ok=True)
 
 crawler.clear_old_files(incoming)
@@ -30,16 +31,17 @@ crawler.killingdup(incoming)
 crawler.excluded(excluded, incoming)
 crawler.blankremover(incoming)
 crawler.sort(incoming)
-maker_rpz.RPZbuilding(excluded, incoming, rpz_locat)
-maker_hosts.hostsbuilding(excluded, incoming, hosts_locat)
-maker_abp.ABPbuilding(excluded, incoming, abp_locat)
-maker_domains.domainsbuilding(excluded, incoming)
+maker_rpz.RPZbuilding(excluded, incoming, rpz_locat ,Version)
+maker_hosts.hostsbuilding(excluded, incoming, hosts_locat ,Version)
+maker_abp.ABPbuilding(excluded, incoming, abp_locat ,Version)
+maker_domains.domainsbuilding(excluded, incoming ,Version)
 
 incoming = "/reprwiki/Private-build/veneto/domains.txt"
 excluded = "/repros/Resources/excluded.txt"
 rpz_locat = "/reprwiki/Private-build/veneto/rpz.txt"
 hosts_locat = "/reprwiki/Private-build/veneto/hosts.txt"
 abp_locat = "/reprwiki/Private-build/veneto/adblock.txt"
+Version = "VENETO"
 os.makedirs('/reprwiki/Private-build/veneto',exist_ok=True)
 
 crawler.clear_old_files(incoming)
@@ -53,10 +55,20 @@ crawler.killingdup(incoming)
 crawler.excluded(excluded, incoming)
 crawler.blankremover(incoming)
 crawler.sort(incoming)
-maker_rpz.RPZbuilding(excluded, incoming, rpz_locat)
-maker_hosts.hostsbuilding(excluded, incoming, hosts_locat)
-maker_abp.ABPbuilding(excluded, incoming, abp_locat)
-maker_domains.domainsbuilding(excluded, incoming)
+maker_rpz.RPZbuilding(excluded, incoming, rpz_locat ,Version)
+maker_hosts.hostsbuilding(excluded, incoming, hosts_locat ,Version)
+maker_abp.ABPbuilding(excluded, incoming, abp_locat ,Version
+maker_domains.domainsbuilding(excluded, incoming ,Version)
+                      
+excluded = "/repros/Resources/excluded.txt"
+Version = "Allowlist"
+rpz_locat = "/reprwiki/Private-build/Allowlist/rpz.txt"
+abp_locat = "/reprwiki/Private-build/Allowlist/adblock.txt"
+domains_locat = "/reprwiki/Private-build/Allowlist/domains.txt"
+maker_rpz.RPZallowlist(excluded, rpz_locat ,Version)
+maker_abp.ABPallowlist(excluded, abp_locat ,Version)
+maker_domains.DMallowlist(excluded ,domains_locat ,Version)
+                      
 
 het = "/repros/version.md"
 version.build(het)

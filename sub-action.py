bcdccd4c5c1771c1deee617dc0b48402bbb10cb5
Minoplhy <zebr@m21.kylz.nl>
2021-08-21 12:56:35 +0700

    Update sub-action.py

--- sub-action.py
+++ sub-action.py
@@ -1,4 +1,9 @@
+import os
+import sys
+
+sys.path.append('/mass-name-change')
 import build_make
+os.makedirs('/gh-releases',exist_ok=True)
 
 build_make.filepath_mass_changer('Allowlist' ,'/reprwiki/Private-build/Allowlist' ,'/gh-releases')
 build_make.filepath_mass_changer('Veneto' ,'/reprwiki/Private-build/veneto' ,'/gh-releases')

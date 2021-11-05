801083582dadb60bc11403f39e21ac0eb6873803
unknown <hej@aspoblourib.1w1.one>
2021-11-05 21:02:56 +0700

    Extension Fix

--- sub-action.py
+++ sub-action.py
@@ -5,6 +5,10 @@ sys.path.append('/filters-maker')
 import build_make
 os.makedirs('/gh-releases',exist_ok=True)
 
-build_make.filepath_mass_changer('Allowlist' ,'/reprwiki/Private-build/Allowlist' ,'/gh-releases')
-build_make.filepath_mass_changer('Veneto' ,'/reprwiki/Private-build/veneto' ,'/gh-releases')
-build_make.filepath_mass_changer('ucate' ,'/reprwiki/Private-build/ucate' ,'/gh-releases')
+build_make.filepath_mass_changer('Allowlist' ,'/reprwiki/Private-build/Allowlist' ,'/gh-releases','txt')
+build_make.filepath_mass_changer('Veneto' ,'/reprwiki/Private-build/veneto' ,'/gh-releases','txt')
+build_make.filepath_mass_changer('ucate' ,'/reprwiki/Private-build/ucate' ,'/gh-releases','txt')
+
+build_make.filepath_mass_changer('Allowlist' ,'/reprwiki/Private-build/Allowlist' ,'/gh-releases','conf')
+build_make.filepath_mass_changer('Veneto' ,'/reprwiki/Private-build/veneto' ,'/gh-releases','conf')
+build_make.filepath_mass_changer('ucate' ,'/reprwiki/Private-build/ucate' ,'/gh-releases','conf')

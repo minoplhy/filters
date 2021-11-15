4bc4546354227cbe76c25780232172bd0fe28e53
unknown <hej@aspoblourib.1w1.one>
2021-11-15 16:18:00 +0700

    source : hBlock-unbound

--- action.py
+++ action.py
@@ -27,7 +27,8 @@ UCATE_SOURCE = [
 'https://github.com/minoplhy/filters/raw/main/Resources/blocked.txt',
 'https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt',
 'https://github.com/DandelionSprout/adfilt/raw/master/Alternate%20versions%20Anti-Malware%20List/AntiMalwareDomains.txt',
-'https://block.energized.pro/basic/formats/filter'
+'https://block.energized.pro/basic/formats/filter',
+'https://hblock.molinero.dev/hosts_unbound.conf'
 ]
 
 crawler.clear_old_files(incoming)
